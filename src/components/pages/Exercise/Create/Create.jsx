02126deca11369551e7c9b1a6fd499929a303314
GuilhermeGz <guilherme.dna06@gmail.com>
2024-02-25 14:50:28 -0300

    Adicionado selects na criação de exercicios e ajustado sua criação

--- src/components/pages/Exercise/Create/Create.jsx
+++ src/components/pages/Exercise/Create/Create.jsx
@@ -1,21 +1,54 @@
-import React, { useState } from 'react';
-import { FaDumbbell, FaTrash, FaPlus } from 'react-icons/fa';
+import React, { useState, useEffect } from 'react';
+import Select from 'react-select';
+import { FaDumbbell, FaPlus } from 'react-icons/fa';
 import "./style.css";
 
 const Create = () => {
     const [exerciseName, setExerciseName] = useState('');
-    const [muscleGroup, setMuscleGroup] = useState('');
-    const [similarExercises, setSimilarExercises] = useState('');
+    const [muscleGroups, setMuscleGroups] = useState([]);
+    const [selectedMuscleGroup, setSelectedMuscleGroup] = useState('');
+    const [similarExercises, setSimilarExercises] = useState([]);
+    const [selectedSimilarExercises, setSelectedSimilarExercises] = useState([]);
+
+    useEffect(() => {
+        const fetchMuscleGroups = async () => {
+            try {
+                const response = await fetch('http://localhost:8000/exercise/api/MuscularGroup');
+                const data = await response.json();
+                setMuscleGroups(data);
+            } catch (error) {
+                console.error('Erro ao buscar os grupos musculares:', error);
+            }
+        };
+
+        fetchMuscleGroups();
+    }, []);
+
+    useEffect(() => {
+        const fetchSimilarExercises = async () => {
+            try {
+                const response = await fetch('http://localhost:8000/exercise/api/Exercise');
+                const data = await response.json();
+                setSimilarExercises(data);
+            } catch (error) {
+                console.error('Erro ao buscar os exercícios semelhantes:', error);
+            }
+        };
+
+        fetchSimilarExercises();
+    }, []);
 
     const handleRequest = () => {
         const exerciseData = {
-            name: "teste",
-            muscularGroupId: "1",
-            equivalentExerciseIds: [],
+            name: exerciseName,
+            muscularGroupId: selectedMuscleGroup,
+            equivalentExerciseIds: selectedSimilarExercises,
             status: "valido",
             trainerId: "inexistente"
         };
 
+        console.log(exerciseData);
+    
         fetch('http://localhost:8000/exercise/api/Exercise', {
             method: 'POST',
             headers: {
@@ -39,8 +72,6 @@ const Create = () => {
         });
     };
 
-
-
     return (
         <div className='main'>
             <div className='title'>
@@ -63,49 +94,57 @@ const Create = () => {
                 <div className='text-input'>
                     <label htmlFor="grupoMuscularInput">
                         <span className='labelTextArea'>Grupo Muscular:</span>
-                        <input
+                        <select
                             className="textArea"
                             id="grupoMuscularInput"
-                            placeholder="Digite aqui"
-                            value={muscleGroup}
-                            onChange={(e) => setMuscleGroup(e.target.value)}
-                        />
+                            value={selectedMuscleGroup}
+                            onChange={(e) => setSelectedMuscleGroup(e.target.value)}
+                        >
+                            <option value="">Selecione um grupo muscular</option>
+                            {muscleGroups.map(group => (
+                                <option key={group.id} value={group.id}>{group.name}</option>
+                            ))}
+                        </select>
                     </label>
                 </div>
                 <div className='text-input'>
                     <label htmlFor="exerciciosSemelhantesInput">
                         <span className='labelTextArea'>Exercícios Semelhantes:</span>
-                        <input
-                            className="textArea"
-                            id="exerciciosSemelhantesInput"
-                            placeholder="Digite aqui"
-                            value={similarExercises}
-                            onChange={(e) => setSimilarExercises(e.target.value)}
+                        <Select className='fonn'
+                            defaultValue={[]}
+                            placeholder="Exercicios Semelhantes"
+                            isMulti
+                            name="exerciciosSemelhantes"
+                            options={similarExercises.map(exercise => ({
+                                value: exercise.id,
+                                label: exercise.name
+                            }))}
+                            value={selectedSimilarExercises.map(exerciseId => ({
+                                value: exerciseId,
+                                label: similarExercises.find(exercise => exercise.id === exerciseId).name
+                            }))}
+                            onChange={(selectedOptions) => {
+                                setSelectedSimilarExercises(selectedOptions.map(option => option.value));
+                            }}
                         />
                     </label>
                 </div>
-
                 <div className="content">
-
                     <div className="cardContainer">
                         <div className="bntAccountContainer">
-                            <FaPlus  className='icon' />
+                            <FaPlus className='icon' />
                             <p className="cardText">Adicionar Exercício</p>
                         </div>
                     </div>
-
-                    <div className="cardContainer">
+                    {/* <div className="cardContainer">
                         <div className="bntAccountContainer">
                             <FaDumbbell className='icon' />
                             <p className="cardText">Exercício 1</p>
                         </div>
-                    </div>
-
-
+                    </div> */}
                 </div>
-
-                <div className="titleSerieSS">
-                    <button className="addSerieButton" onClick={handleRequest}>
+                <div className="serie_btns">
+                    <button type='submit' className="concluirButton" onClick={handleRequest}>
                         <span>Solicitar</span>
                     </button>
                 </div>
