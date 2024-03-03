cfc015fe3cd5e4010e188f26beee7e051cddb21f
GuilhermeGz <guilherme.dna06@gmail.com>
2024-03-03 19:28:29 -0300

    Ajuste de rota de cancelar em preset

--- src/components/pages/Training/Preset/Create/Create.jsx
+++ src/components/pages/Training/Preset/Create/Create.jsx
@@ -10,7 +10,7 @@ const Create = () => {
   const [inputName, setInputName] = useState("");
 
   const cancelButtonClick = () => {
-    navigate(`Training/Preset`);
+    navigate(`/Training/Preset`);
   };
 
   return (
